f371721bf9adf3b6b515e56c0f9d649df99abdaf
e2molin <esteban.emolin@gmail.com>
2021-11-29 17:55:42 +0100

    Recuperado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';  //Así podemos utilizar JSX
 import ReactDOM from 'react-dom';
-import App from 'App';
+import { App } from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+//ReactDOM.render(<div>Hic sunt dracones</div>,document.getElementById("root"));
+ReactDOM.render(<App />,document.getElementById("root"))
